4ba9d07b86a9180958092cd0567d882df619c3e1
erick guzman <156051814+ErickGuuz@users.noreply.github.com>
2024-04-25 12:02:19 -0400

    Update fasta_seq_count.sh
    REDO FOR BETTER GRADE

--- fasta_seq_count.sh
+++ fasta_seq_count.sh
@@ -4,9 +4,9 @@ echo "This script can count the number of sequences in a fasta file."
 greeting="Hello"
 your_name="Erick!"
 echo "$greeting" "$your_name"
-mkdir bioinformatics
-cd bioinformatics
-mkdir learning_bash
+mkdir ~/Desktop/bioinformatics
+cd ~/Desktop/bioinformatics
+mkdir ~Desktop/learning_bash
 cd ~/Desktop
 mv transcriptome1.fasta ~/Desktop/bioinformatics/learning_bash
 cd ~/Desktop/bioinformatics/learning_bash 
